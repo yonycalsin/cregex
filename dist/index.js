a1074811b2fa888048b966ddfe92a8b1b4af7050
yonicb <yonicb20@gmail.com>
2020-01-01 10:07:14 -0500

    ci: 🎡 Buil main file

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,1 @@
-/*!
- * Cregex v1.0.0 (https://github.com/yonicb/cregex)
- * Copyright 2019 The Cregex Authors
- * Copyright 2019 Yoni Calsin <@helloyonicb@gmail.com>.
- * Licensed under MIT (https://github.com/yonicb/cregex/blob/master/LICENSE)
- */
-var hello=function(l){console.log("Hello "+l)};
+var hello=function(l){console.log("Hello "+l)};exports=hello;
